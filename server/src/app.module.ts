99e4624529047e9cfef1069e271d89b7c8407552
Eyüp Karataş <eyupkaratasbusiness@gmail.com>
2025-09-18 03:21:19 +0300

    feat(products):create product.module & add to app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
